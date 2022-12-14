b117a4393a14f420818c880a0abc6ba035244051
osacrMesh <okerekeifeanyi65@gmail.com>
2022-12-14 17:48:50 +0100

    http-server

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
-const http = require("http");
+
+const http = require('http');
 
 const PORT = 3000;
+
 const server = http.createServer();
+
 const friends = [
   {
     id: 0,
-    name: "John",
+    name: 'Nikola Tesla',
   },
   {
     id: 1,
-    name: "Jane",
+    name: 'Sir Isaac Newton',
   },
   {
     id: 2,
-    name: "Jack",
-  },
+    name: 'Albert Einstein',
+  }
 ];
 
-server.on("request", (req, res) => {
-  const items = req.url.split("/");
-
-  if (items[1] === "friends") {
+server.on('request', (req, res) => {
+  const items = req.url.split('/');
+  // /friends/2 => ['', 'friends', '2']
+  // /friends/
+  if (req.method === 'POST' && items[1] === 'friends') {
+    req.on('data', (data) => {
+      const friend = data.toString();
+      console.log('Request:', friend);
+      friends.push(JSON.parse(friend));
+    });
+    req.pipe(res);
+  } else if (req.method === 'GET' && items[1] === 'friends') {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-
+    res.setHeader('Content-Type', 'application/json');
     if (items.length === 3) {
       const friendIndex = Number(items[2]);
       res.end(JSON.stringify(friends[friendIndex]));
     } else {
       res.end(JSON.stringify(friends));
     }
-  } else if (items[1] === "/messages") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<body>");
-    res.write("<ul>");
-    res.write("<li>Hello World</li>");
-    res.write("<li>What is up?</li>");
-    res.write("</ul>");
-    res.write("</body>");
-    res.write("</html>");
+  } else if (req.method === 'GET' && items[1] === 'messages') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Isaac!</li>');
+    res.write('<li>What are your thoughts on astronomy?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
     res.end();
   } else {
     res.statusCode = 404;
@@ -48,5 +58,5 @@ server.on("request", (req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Listening on port ${PORT}...`);
+}); //127.0.0.1 => localhost
